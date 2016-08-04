c7eaa711de51cb64511447e089aa6360b89b1c08
RanjanaBhat <ranjana.bhat@anchanto.com>
2016-08-04 16:04:02 +0530

    "GitMavenDemo"

---
+++ GitDemo/src/test/java/GitDemo/GitDemo/Demo.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package GitDemo.GitDemo;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author anchanto
+ *
+ */
+public class Demo {
+	@Test
+	public void testDemo(){
+		System.out.println("Hello Jenkins Git..!");
+	}
+}
